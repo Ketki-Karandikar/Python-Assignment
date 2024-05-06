448fd37ea2949a9c07c611101389b6c1294d520b
Ketki-Karandikar <167843853+Ketki-Karandikar@users.noreply.github.com>
2024-05-06 09:51:10 +0530

    Update question--8.py

--- Assignments/Module_2/question--8.py
+++ Assignments/Module_2/question--8.py
@@ -1,13 +1,14 @@
 # Write a python program to test whether a passed letter is a vowel or not ?
 
 
-# Function to check whether a character is vowel or consonant 
+# Check whether a character is vowel or consonant 
 
-x = (input("Enter letter : "))
-if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'):   # vowels
-  print("Vowel") 
+K = (input("Enter letter : "))
+if (K == 'a' or K == 'e' or K == 'i' or K == 'o' or K == 'u'):   # vowels
+  print("Vowel")
+
+elif (K == 'A' or K == 'E' or K == 'I' or K == 'O' or K == 'U'):   # vowels
+  print("vowel")
+
 else:
   print("Consonant") 
-
-
-
