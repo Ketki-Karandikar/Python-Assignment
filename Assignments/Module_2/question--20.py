a828d6213346892114b9c41c313c84e13ba67f61
Ketki-Karandikar <167843853+Ketki-Karandikar@users.noreply.github.com>
2024-05-06 11:36:27 +0530

    Update question--20.py

--- Assignments/Module_2/question--20.py
+++ Assignments/Module_2/question--20.py
@@ -1,11 +1,17 @@
 # Write a Python function that takes a list of words and returns the length of the longest one.
 
-def longestLength(words):
-    str=max(words,key=len)       # To calculate the maximum length
-    print("The word with the longest length is:", str,
-          " and length is ", len(str))
- 
- 
-a = ["one", "two", "third", "hundred"]    # Array of words
-longestLength(a)    # To know the longest length of the words
+def find_longest_word(words_list):   # Define the longest word
+    word_len = []   # Blank list 
 
+    for n in words_list:   # for n number of words in a given list
+        
+        word_len.append((len(n), n))    # append to add words in a list
+
+    word_len.sort()   # sort for sorting the list of words
+
+    return word_len[-1][0], word_len[-1][1]    # to return the length of the words given in a list
+
+result = find_longest_word(["Macroni", "Red Souce Pasta", "IceCream"])   # give the input
+
+print("\nLongest word: ", result[1])   # To print the longest word
+print("Length of the longest word: ", result[0])  # To print the Length of the longest word
